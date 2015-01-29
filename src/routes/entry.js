1ca20790ace60217c79f0648f36d4e4afeb74026
Michaël Zasso <mic.besace@gmail.com>
2015-01-29 08:46:29 +0100

    add GET attachment

--- src/routes/entry.js
+++ src/routes/entry.js
@@ -3,26 +3,61 @@
 var Router = require('koa-router');
 var middleware = require('../middleware');
 
+var validateEntry = middleware.validateParameters([
+    { name: 'entryId', type: 'objectid' }
+]);
+var validateAttachment = middleware.validateParameters([
+    { name: 'entryId', type: 'objectid' },
+    { name: 'attachmentId', type: 'objectid' }
+]);
+
 module.exports = function (hds) {
 
     var router = new Router();
 
-    router.get('/:kind/:entryId', middleware.validateParameters([{type: 'objectid', name: 'entryId'}]), getEntry);
+    router.get('/:kind/:entryId', validateEntry, checkKind, checkEntry, getEntry);
+
+    router.get('/:kind/:entryId/:attachmentId', validateAttachment, checkKind, checkEntry, getAttachment);
 
     return router.middleware();
 
-    function*getEntry() {
+    function*checkKind(next) {
         try {
-            var kind = yield hds.Kind.get(this.params.kind);
-            var entry = yield kind.findOne({_id: this.params.entryId}).exec();
-            if (entry) {
-                this.body = entry;
-            } else {
-                this.hds_jsonError(404, 'entry ' + this.params.entryId + ' not found');
-            }
+            this.state.hds_kind = yield hds.Kind.get(this.params.kind);
         } catch (e) {
-            this.hds_jsonError(404, 'kind ' + this.params.kind + ' not found');
+            return this.hds_jsonError(404, 'kind ' + this.params.kind + ' not found');
         }
+        yield next;
+    }
+
+    function*checkEntry(next) {
+        var entry = yield this.state.hds_kind.findOne({_id: this.params.entryId}).exec();
+        if (entry) {
+            this.state.hds_entry = entry;
+            yield next;
+        } else {
+            this.hds_jsonError(404, 'entry ' + this.params.entryId + ' not found');
+        }
+    }
+
+    function*getEntry() {
+        var entry = this.state.hds_entry.toObject();
+        if (entry._at) {
+            var i = 0, l = entry._at.length, at;
+            for (; i < l; i++) {
+                at = entry._at[i];
+                at.url = this.state.hds_host + 'entry/' + this.params.kind + '/' + entry._id + '/' + at._id;
+            }
+        }
+        this.body = entry;
+    }
+
+    function*getAttachment() {
+        var entry = this.state.hds_entry;
+        var att = yield entry.getAttachment(this.params.attachmentId);
+        this.set('Content-Type', att.mime);
+        this.set('Content-Disposition', 'attachment;filename="' + att.name + '"');
+        this.body = att.content;
     }
 
 };
--- test/hds/kind.js
+++ test/hds/kind.js
@@ -15,13 +15,25 @@ FakeKind.findOne = function (conditions) {
     return {
         exec: function () {
             if (conditions._id === constants.ENTRY_ID_EXIST) {
-                return Promise.resolve({
+                return Promise.resolve(mongoosify({
                     _id: constants.ENTRY_ID_EXIST,
                     field1: 'hello'
-                });
+                }));
             } else {
                 return Promise.resolve(null);
             }
-        }
+        },
+        lean: returnThis
     };
 };
+
+function returnThis() {
+    return this;
+}
+
+function mongoosify(object) {
+    object.toObject = function () {
+        return this;
+    };
+    return object;
+}
