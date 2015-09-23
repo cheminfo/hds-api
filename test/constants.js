c2b479b6919c2fef90c1bc7f00af4313391709cf
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2015-09-23 10:49:44 -0500

    test: trying to implement first test

--- test/constants.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-exports.KIND_EXIST = 'kind1';
-exports.KIND_NOTEXIST = 'kind2';
-
-exports.ENTRY_ID_EXIST = '000000000000000000000000';
-exports.ENTRY_ID_NOTEXIST = '000000000000000000000001';
-
-exports.NEW_ENTRY_KINDEXIST = { _id: '123' ,field1: 'Hello two'};
