cba2ca71f8615d9e55fc7d177d4c80a10fff48cc
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2015-10-20 00:21:04 -0500

    feat(kind): get kind

--- src/routes/kind.js
+++ src/routes/kind.js
@@ -11,6 +11,7 @@ module.exports = function (hds) {
     router.post('/_new/:kind', middleware.checkUser, createKind);
     router.put('/:kind', middleware.checkKind, middleware.checkUser, updateKind);
     router.delete('/:kind', middleware.checkKind, middleware.checkUser, deleteKind);
+    router.get('/:kind', middleware.checkKind, middleware.checkUser, getKind);
 
     return router.middleware();
 
@@ -48,6 +49,14 @@ module.exports = function (hds) {
         try {
             yield this.state.hds_kind.remove();
             this.body = {status: 'deleted'};
+        } catch (err) {
+            this.hds_jsonError(500, err);
+        }
+    }
+
+    function* getKind() {
+        try {
+            this.body = this.state.hds_kind;
         } catch (err) {
             this.hds_jsonError(500, err);
         }
